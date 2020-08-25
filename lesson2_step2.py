c8920cb3e5890c0ae7c054f399f3ec9fcb4ba548
Darya <gobalticdasha@gmail.com>
2020-08-25 10:28:15 +0300

    files lesson2 added

---
+++ lesson2_step2.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import time
+import os
+
+try:
+    link = "http://suninjuly.github.io/file_input.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element_by_css_selector("div.form-group input[name = firstname]")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_css_selector("div.form-group input[name = lastname]")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_css_selector("div.form-group input[name = email]")
+    input3.send_keys("gobalticdasha@gmail.com")
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))    # получаем путь к директории текущего исполняемого файла
+    file_path = os.path.join(current_dir, 'file.txt')           # добавляем к этому пути имя файла
+    element = browser.find_element_by_id('file')
+    element.send_keys(file_path)
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_class_name("btn.btn-primary")
+    button.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
---
+++ lesson2_step4.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_class_name("trollface.btn.btn-primary")
+    button.click()
+
+    #перейти на новую вкладку
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    #решить задание
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_class_name("btn.btn-primary")
+    button.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
